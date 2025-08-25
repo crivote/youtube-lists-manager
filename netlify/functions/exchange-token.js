824a273f537f37b23c01fa963fd2077f08ad1c5c
Victor Gomez <crivote@gmail.com>
2025-08-25 20:23:49 +0200

    improve netlify function to handle refresh token

--- netlify/functions/exchange-token.js
+++ netlify/functions/exchange-token.js
@@ -1,17 +1,26 @@
 // netlify/functions/exchange-token.js
 
 const { google } = require("googleapis");
+const CLIENT_ID = process.env.VITE_GOOGLE_CLIENT_ID;
+const CLIENT_SECRET = process.env.VITE_GOOGLE_CLIENT_SECRET;
+const REDIRECT_URI = "postmessage";
 
 exports.handler = async function (event, context) {
   if (event.httpMethod !== "POST") {
-    return { statusCode: 405, body: "Método no permitido" };
+    return { statusCode: 405, body: "Non allowed method" };
+  }
+  if (!event.body) {
+    return { statusCode: 400, body: "Missing request body" };
+  }
+  const { code, refreshToken } = JSON.parse(event.body);
+  if (!code && !refreshToken) {
+    return {
+      statusCode: 400,
+      body: "Missing authorization code or refresh token",
+    };
   }
 
-  const { code } = JSON.parse(event.body);
-
-  const CLIENT_ID = process.env.VITE_GOOGLE_CLIENT_ID;
-  const CLIENT_SECRET = process.env.VITE_GOOGLE_CLIENT_SECRET;
-  const REDIRECT_URI = "postmessage";
+  console.log("Código de autorización recibido. Longitud:", code.length);
 
   const oauth2Client = new google.auth.OAuth2(
     CLIENT_ID,
@@ -20,16 +29,56 @@ exports.handler = async function (event, context) {
   );
 
   try {
-    const { tokens } = await oauth2Client.getToken(code);
+    if (refreshToken) {
+      console.log("Renovando token...");
+      oauth2Client.setCredentials({ refresh_token: refreshToken });
+      const { tokens } = await oauth2Client.refreshAccessToken();
 
-    // Puedes decidir si guardar el refresh token en una base de datos o en el navegador.
-    // Para esta prueba, lo devolvemos al cliente.
-    return {
-      statusCode: 200,
-      body: JSON.stringify(tokens),
-    };
+      console.log(
+        "Token renovado con éxito. Nuevo access token:",
+        tokens.access_token
+      );
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify(tokens),
+      };
+    }
+
+    if (code) {
+      console.log("Intercambiando código por tokens...");
+      const { tokens } = await oauth2Client.getToken(code);
+      console.log(
+        "Tokens obtenidos con éxito. Expiración:",
+        tokens.expiry_date
+      );
+
+      // Configura el cliente de Google con el access_token
+      oauth2Client.setCredentials(tokens);
+
+      // Llama a la API de Google People para obtener los datos del usuario
+      const people = google.people({ version: "v1", auth: oauth2Client });
+      const profile = await people.people.get({
+        resourceName: "people/me",
+        personFields: "names,emailAddresses,photos",
+      });
+      console.log(
+        "Perfil del usuario obtenido:",
+        profile.data.names[0].displayName
+      );
+
+      // Puedes decidir si guardar el refresh token en una base de datos o en el navegador.
+      // Para esta prueba, lo devolvemos al cliente.
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          tokens,
+          userProfile: profile.data,
+        }),
+      };
+    }
   } catch (error) {
-    console.error("Error al intercambiar el código:", error);
+    console.error("Error al intercambiar el código:", error.message);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: "Error al obtener los tokens" }),
